333b88c1398f6e30626e32b860a9137ceac083ac
LANCELLE Xavier (SNCF VOYAGEURS / DIRECTION DE LIGNES ALPES / Siège UO Servi CHAMBERY) <xavilanc@gmail.com>
2022-09-07 14:05:43 +0200

    begin search area

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,11 @@
-import Counter from "@components/Counter";
 import Header from "@components/Header";
+import SearchTrain from "@components/SearchTrain";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <SearchTrain />
     </div>
   );
 }
