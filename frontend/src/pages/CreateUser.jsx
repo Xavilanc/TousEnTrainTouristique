7ef8cb837703b6f3d0f94674c54bd5ff11acd5f3
LANCELLE Xavier (SNCF VOYAGEURS / DIRECTION DE LIGNES ALPES / Siège UO Servi CHAMBERY) <xavilanc@gmail.com>
2022-09-08 14:56:20 +0200

    create user form component

---
+++ frontend/src/pages/CreateUser.jsx
@@ -0,0 +1,15 @@
+import Header from "@components/Header";
+import CreateUserForm from "@components/CreateUserForm";
+import Footer from "@components/Footer";
+
+function CreateUser() {
+  return (
+    <div>
+      <Header />
+      <CreateUserForm />
+      <Footer />
+    </div>
+  );
+}
+
+export default CreateUser;
