4997c1de24cc0b832fddfcbafa0cffd8932d1b2e
Sonia34sonia <sonia.vidal@sncf.fr>
2022-09-08 12:15:27 +0200

    page contact + css

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -2,47 +2,59 @@
 import "@assets/styles/Contact.css";
 import React, { useState } from "react";
 
-function Contact() {
+export default function Contact() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
 
   function handleSubmit(e) {
     e.preventDefault();
+    alert(`Votre message a bien été envoyé ${name}!`);
   }
 
   return (
-    <div className="Contact">
-      <h1>Contact</h1>
-      <form onSubmit={handleSubmit}>
-        <div>*champs obligatoires</div>
-        <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          id="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <label htmlFor="message">Message</label>
-        <textarea
-          name="message"
-          id="message"
-          cols="30"
-          rows="10"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-        />
-        <button type="submit">Envoyer</button>
-        <button type="submit">Annuler</button>
-      </form>
+    <div className="contact1">
+      <div className="Contact">
+        <h1>Contact</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="champs">*champs obligatoires</div>
+
+          <input
+            className="name"
+            type="text"
+            id="name"
+            value={name}
+            placeholder="Nom et Prénom*"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            className="email"
+            type="email"
+            id="email"
+            value={email}
+            placeholder="Adresse e-mail*"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <textarea
+            className="message"
+            name="message"
+            id="message"
+            cols="30"
+            rows="10"
+            value={message}
+            placeholder="Votre message*"
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <div className="buttonsContainer">
+            <button className="buttonForm" type="submit">
+              Envoyer
+            </button>
+            <button className="buttonForm1" type="submit">
+              Annuler
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
-export default Contact;
