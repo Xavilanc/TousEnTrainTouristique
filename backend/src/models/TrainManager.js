7bc715b752ab29ea4e572ceb1336bbdcac261b3e
v3ry <v3ryfr@gmail>
2022-09-08 13:44:55 +0200

    adding put for train

--- backend/src/models/TrainManager.js
+++ backend/src/models/TrainManager.js
@@ -7,15 +7,40 @@ class TrainManager extends AbstractManager {
 
   insert(train) {
     return this.connection.query(
-      `insert into ${this.table} (title) values (?)`,
-      [train.title]
+      `insert into ${this.table} (name, train_user_id, area_id, created_on, updated_on, published, description, description_info) values (?, ?, ?, ?, ?, ?, ?, ?)`,
+      [
+        train.name,
+        train.train_user_id,
+        train.area_id,
+        train.created_on,
+        train.updated_on,
+        train.published,
+        train.description,
+        train.description_info,
+      ]
     );
   }
 
+  // update(train) {
+  //   return this.connection.query(
+  //     `update ${this.table} set title = ? where id = ?`,
+  //     [train.name, train.id]
+  //   );
+  // }
   update(train) {
     return this.connection.query(
-      `update ${this.table} set title = ? where id = ?`,
-      [train.title, train.id]
+      `update ${this.table} set name = ?, train_user_id = ?, area_id = ?, created_on = ?, updated_on = ?, published = ?, description = ?, description_info = ? where id = ?`,
+      [
+        train.name,
+        train.train_user_id,
+        train.area_id,
+        train.created_on,
+        train.updated_on,
+        train.published,
+        train.description,
+        train.description_info,
+        train.id,
+      ]
     );
   }
 }
