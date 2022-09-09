09b0b46b08d78cb1b1ba41a8a709d13efefa6e24
Sonia34sonia <sonia.vidal@sncf.fr>
2022-09-09 09:46:02 +0200

    modification link sur logo

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,13 @@
 import logoblanc from "@assets/images/logo-blanc.png";
 import "@assets/styles/Header.css";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className="header_main_container">
-      <img className="header_logo" src={logoblanc} alt="logo du site" />
+      <Link to="/">
+        <img className="header_logo" src={logoblanc} alt="logo du site" />
+      </Link>
       <div className="header_links_title_container">
         <div className="header_links_container">
           <div className="header_links">M'inscrire</div>
