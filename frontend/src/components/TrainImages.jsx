0ca7d69439abc2fe4d31c75af1286aa4cd36562d
LANCELLE Xavier (SNCF VOYAGEURS / DIRECTION DE LIGNES ALPES / Siège UO Servi CHAMBERY) <xavilanc@gmail.com>
2022-10-11 14:30:45 +0200

    fix css bugs after deploy gna gna gna

--- frontend/src/components/TrainImages.jsx
+++ frontend/src/components/TrainImages.jsx
@@ -2,10 +2,10 @@ import "../assets/styles/TrainImages.css";
 
 function TrainImages({ images }) {
   return (
-    <div>
+    <div className="train_image_image_div">
       {images &&
         images.map((image) => (
-          <div key={image.id}>
+          <div className="train_image_title_box" key={image.id}>
             <img className="train_image" src={image.path} alt={image.title} />
             <div className="train_image_title">{image.title}</div>
           </div>
